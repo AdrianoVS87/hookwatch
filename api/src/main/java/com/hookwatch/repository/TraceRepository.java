acf6428698155e3b6acb0b28c2e6b451f5132b04
Adriano Viera dos Santos <adrianovs.info@gmail.com>
2026-03-23 01:30:49 +0000

    perf(api): add @EntityGraph to eliminate N+1 on paginated trace list

--- api/src/main/java/com/hookwatch/repository/TraceRepository.java
+++ api/src/main/java/com/hookwatch/repository/TraceRepository.java
@@ -3,6 +3,7 @@ package com.hookwatch.repository;
 import com.hookwatch.domain.Trace;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Repository
 public interface TraceRepository extends JpaRepository<Trace, UUID> {
 
+    @EntityGraph(attributePaths = {"spans"})
     Page<Trace> findByAgentId(UUID agentId, Pageable pageable);
 
     @Query("""
